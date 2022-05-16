e3164572d43375ffe56813e18d5e150d91f87cfe
Juan Burgos <juanlb1988@gmail.com>
2022-05-16 19:49:29 -0400

    Fix screenrc soft link

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 [ -f ${HOME}/.vimrc ] || ln -s ${PWD}/vim/vimrc ${HOME}/.vimrc
-[ -f ${HOME}/.screenrc ]  || ln -s ${PWD}/screenrc/screenrc ${HOME}/.screenrc
+[ -f ${HOME}/.screenrc ]  || ln -s ${PWD}/screen/screenrc ${HOME}/.screenrc
 [ -f ${HOME}/.tmux.conf ] || ln -s ${PWD}/tmux/tmux.conf ${HOME}/.tmux.conf
 [ -d ${HOME}/.config/nvim ] || ln -s ${PWD}/nvim ${HOME}/.config/nvim
