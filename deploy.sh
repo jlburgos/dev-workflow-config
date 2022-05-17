ff88e2e577ea159cf31d9d43e82ac31f4159cd87
root <root@jlburgos-instana1.fyre.ibm.com>
2022-05-17 12:30:45 -0700

    Adding mkdir for $HOME/.config since it seems to be missing sometimes...

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,7 @@ set -x
 [ -f ${HOME}/.vimrc ] || ln -s ${PWD}/vim/vimrc ${HOME}/.vimrc
 [ -f ${HOME}/.screenrc ]  || ln -s ${PWD}/screen/screenrc ${HOME}/.screenrc
 [ -f ${HOME}/.tmux.conf ] || ln -s ${PWD}/tmux/tmux.conf ${HOME}/.tmux.conf
-[ -d ${HOME}/.config/nvim ] || ln -s ${PWD}/nvim ${HOME}/.config/nvim
+[ -d ${HOME}/.config/nvim ] || {
+  [ -d ${HOME}/.config ] || mkdir -p ${HOME}/.config
+  ln -s ${PWD}/nvim ${HOME}/.config/nvim
+}
