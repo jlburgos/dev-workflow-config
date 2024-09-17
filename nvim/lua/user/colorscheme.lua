dc08469de58251de4bcdaacd6ca12228a9aafd8b
Juan Burgos <jlburgos@us.ibm.com>
2024-09-17 13:32:58 -0500

    Add transparency

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -11,3 +11,11 @@ if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
+
