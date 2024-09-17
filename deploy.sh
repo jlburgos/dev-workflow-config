1b29ea8840e19f70118ea7e3bca2063390834a50
Juan Burgos <jlburgos@us.ibm.com>
2024-09-17 13:32:33 -0500

    Update deploy script to check cwd and for better organization

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+## Should run script from current directory
+current_dir="$(realpath $(pwd))"
+script_dir="$(dirname "$(realpath "$0")")"
+[ "$current_dir" == "$script_dir" ] || {
+  echo "Error: This script must be run from the directory it is located in."
+  echo "Current Directory: ${current_dir}"
+  echo "Script  Directory: ${script_dir}"
+  exit 1
+}
+
 set -o xtrace
 
+## Common git stuff
 [ -f ${HOME}/.gitconfig ] || ln -s ${PWD}/git/gitconfig ${HOME}/.gitconfig
-[ -f ${HOME}/.vimrc ] || ln -s ${PWD}/vim/vimrc ${HOME}/.vimrc
+
+## Terminal multiplexer stuff
 [ -f ${HOME}/.screenrc ]  || ln -s ${PWD}/screen/screenrc ${HOME}/.screenrc
 [ -f ${HOME}/.tmux.conf ] || ln -s ${PWD}/tmux/tmux.conf ${HOME}/.tmux.conf
+
+## Favorite editor stuff
+[ -f ${HOME}/.vimrc ] || ln -s ${PWD}/vim/vimrc ${HOME}/.vimrc
 [ -d ${HOME}/.config/nvim ] || {
-  [ -d ${HOME}/.config ] || mkdir -p ${HOME}/.config
+  mkdir -p ${HOME}/.config
   ln -s ${PWD}/nvim ${HOME}/.config/nvim
 }
 
-[ -f ${HOME}/.oh-my-zsh/custom/themes/custom-ys.zsh-theme ] || {
-  ln -s ${PWD}/zsh/custom-ys.zsh-theme ${HOME}/.oh-my-zsh/custom/themes/custom-ys.zsh-theme 
+## Shell stuff
+[ -f ${HOME}/.config/oh-my-zsh/custom/themes/custom-ys.zsh-theme ] || {
+  [ -d ${HOME}/.config/oh-my-zsh] || ln -s ${HOME}/.oh-my-zsh ${HOME}/.config/oh-my-zsh
+  ln -s ${PWD}/zsh/custom-ys.zsh-theme ${HOME}/.config/oh-my-zsh/custom/themes/custom-ys.zsh-theme 
 }
